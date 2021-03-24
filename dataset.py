5eadabec938beaf29c77a193749cb8ce8d8bda5c
Anton <acheshkov@gmail.com>
2021-03-24 18:51:33 +0300

    First step of pipeline. Prepare data for firther experiment (#7)
    * fixed informatino in README
    
    * added test for 'combine_all_together'
    
    * flake complience
    
    * fixed review comments

--- dataset.py
+++ dataset.py
@@ -2,20 +2,11 @@
 import pandas as pd
 import os
 from config import Config
-from typing import Tuple
+from typing import Tuple, Optional
 from type_aliases import Path
-from type_aliases import RangesDataset, EmosVectorsDataset, SynthDataset, Dataset
+from type_aliases import EmosVectorsDataset, SynthDataset, Dataset
 from sklearn.model_selection import train_test_split
 from source_code_utils import complement_range_file
-
-
-def get_emos_ranges(path_to_csv: Path) -> RangesDataset:
-    ''' Load and prepare dataset with calculated possible EMO ranges '''
-    emos_ranges = pd \
-        .read_csv(path_to_csv, header=None, names=['filename', 'class_name', 'method_name', 'ranges']) \
-        .drop_duplicates(['filename'], keep=False)
-    emos_ranges.filename = emos_ranges.filename.apply(os.path.basename)
-    return emos_ranges
 
 
 def get_emos_vectors(path_to_csv: Path) -> EmosVectorsDataset:
@@ -23,7 +14,7 @@ def get_emos_vectors(path_to_csv: Path) -> EmosVectorsDataset:
     def _mk_link_to_original_sample(filename) -> str:
         return '_'.join(filename.split('_')[0: 4]) + '.java'
 
-    def _ex_range(filename) -> str:
+    def _extract_range(filename) -> str:
         out = list(map(int, filename.replace('.java', '').split('_')[-2:]))
         return str(out)
 
@@ -32,11 +23,12 @@ def get_emos_vectors(path_to_csv: Path) -> EmosVectorsDataset:
         .drop_duplicates(['emo_uid'], keep=False)
     emos_vectors.emo_uid = emos_vectors.emo_uid.apply(os.path.basename)
     emos_vectors['filename_origin'] = emos_vectors.emo_uid.apply(_mk_link_to_original_sample)
-    emos_vectors['range'] = emos_vectors.emo_uid.apply(_ex_range)
+    emos_vectors['range'] = emos_vectors.emo_uid.apply(_extract_range)
     return emos_vectors
 
 
-def _figure_out_true_inline_range(row: pd.core.series.Series) -> str:
+def _figure_out_true_inline_range(row: pd.core.series.Series) -> Optional[str]:
+    '''Figure out and returns True range as a string'''
     start = int(row['insertion_start']) - 1
     end = int(row['insertion_end']) - 1
     return None
@@ -90,18 +82,40 @@ def get_synth_dataset(path_to_dataset_csv: Path, path_to_java_files: Path) -> Sy
 
 
 def combine_all_together(emos: EmosVectorsDataset, synth: SynthDataset) -> Dataset:
-    pass
+    columns = [
+        'filename', 'true_inline_range',
+        'output_filename', 'target_method', 'target_method_start_line',
+        'project_id', 'vector_str', 'range', 'emo_uid'
+    ]
+    join = synth.merge(emos, 'inner', left_on='filename', right_on='filename_origin')[columns].values
+    data = []
+
+    for fn, true_range, fn_full, method, target_method_start_line, project, vector_str, rrange, emo_uid in join:
+        is_true_range = true_range == rrange
+        rrange = eval(rrange)
+        data.append((
+            fn_full, emo_uid, vector_str, project,
+            is_true_range,
+            rrange[1] - rrange[0],
+            method, target_method_start_line,
+            str(rrange), true_range
+        ))
+
+    columns = [
+        'filename', 'emo_uid', 'vector', 'project', 'is_true_emo', 'range_len',
+        'target_method', 'target_method_start_line', 'range', 'true_range'
+    ]
+    df = pd.DataFrame(data, columns=columns)
+    return df
 
 
 def get_dataset(config: Config) -> pd.DataFrame:
     '''Load, preprocess and combine all input data parts together to form a single dataset'''
     synth = get_synth_dataset(config.path_to_dataset_csv, config.path_to_java_files)
     print("Synth dataset:", len(synth))
-    emos_ranges = get_emos_ranges(config.path_to_ranges_csv)
-    print("Ranges dataset:", len(emos_ranges))
     emos_vectors = get_emos_vectors(config.path_to_emos_vectors)
     print("EMOS vectors dataset:", len(emos_vectors))
-    ds = combine_all_together(emos_ranges, emos_vectors, synth)
+    ds = combine_all_together(emos_vectors, synth)
     print("Final dataset:", len(ds))
     return ds
 
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,10 +1,44 @@
 import unittest
 import pandas as pd
-from dataset import split_dataset
+import tempfile
+from dataset import split_dataset, get_emos_vectors, combine_all_together
+from dataset import SynthDataset, EmosVectorsDataset
 from dataset import _drop_duplicates_by_filename, _drop_rows_with_undefined_true_range, _mk_full_path_to_java_files
 
 
 class TestDataset(unittest.TestCase):
+
+    def _mock_synth_dataset(self) -> SynthDataset:
+        columns = [
+            'output_filename',
+            'insertion_start',
+            'insertion_end',
+            'class_name',
+            'target_method',
+            'target_method_start_line',
+            'project_id',
+            'filename',
+            'true_inline_range'
+        ]
+        return pd.DataFrame(
+            [
+                ('/path/to/file_1.java', '40', '59', 'Foo', 'getItem', '34', 'apache/netbeans', 'file_1.java', '[40, 60]'),
+                ('/path/to/file_2.java', '3', '6', 'Boo', 'subscribe', '2', 'apache/netbeans', 'file_2.java', '[3, 10]')
+            ],
+            columns=columns
+        )
+
+    def _mock_emos_vectors_dataset(self) -> EmosVectorsDataset:
+        columns = ['emo_uid', 'filename_origin', 'range', 'vector_str']
+        return pd.DataFrame(
+            [
+                ('/emo/uid/1', 'file_1.java', '[40, 60]', '-0.1 0.3 -0.9'),
+                ('/emo/uid/2', 'file_1.java', '[40, 50]', '-0.2 0.3 -0.9'),
+                ('/emo/uid/3', 'file_2.java', '[5, 7]', '-0.1 0.3 -0.8'),
+                ('/emo/uid/4', 'file_2.java', '[4, 7]', '-0.1 0.2 -0.9'),
+            ],
+            columns=columns
+        )
 
     def test_split_dataset(self):
         df = pd.DataFrame(
@@ -18,11 +52,36 @@ class TestDataset(unittest.TestCase):
         self.assertEqual(len(test.query("project == 'A'")), 25)
         self.assertEqual(len(test.query("project == 'B'")), 1)
 
-    def test_get_emos_ranges(self):
-        pass
-
     def test_get_emos_vectors(self):
-        pass
+        with tempfile.NamedTemporaryFile(mode='w+') as temp_csv:
+            temp_csv.write(','.join(['/path/to/p1_p2_p3_p4_p5_3_8.java', '-0.1 0.3 -0.9']))
+            temp_csv.write(','.join(['/path/to/p1_p2_p3_p4_p5_3_8.java', '-0.1 0.3 -0.9']))
+            temp_csv.write(','.join(['/path/to/p1_p2_p3_p4_p5_5_7.java', '-0.3 -0.3 0.2']))
+            temp_csv.seek(0)
+            emos_vectors = get_emos_vectors(temp_csv.name)
+            self.assertIsInstance(emos_vectors, pd.DataFrame)
+            self.assertIn('vector_str', emos_vectors.columns)
+            self.assertEqual(len(emos_vectors), 1)
+            self.assertEqual(emos_vectors.range[0], '[5, 7]')
+            self.assertEqual(emos_vectors.filename_origin[0], 'p1_p2_p3_p4.java')
+            self.assertEqual(emos_vectors.emo_uid[0], 'p1_p2_p3_p4_p5_5_7.java')
+
+    def test_combine_all_together(self):
+        emos_vectors: EmosVectorsDataset = self._mock_emos_vectors_dataset()
+        synth: SynthDataset = self._mock_synth_dataset()
+        dataset = combine_all_together(emos_vectors, synth)
+
+        columns = [
+            'filename', 'emo_uid', 'vector', 'project', 'is_true_emo', 'range_len',
+            'target_method', 'target_method_start_line', 'range', 'true_range'
+        ]
+        for col_name in columns:
+            self.assertIn(col_name, dataset.columns)
+        self.assertIsInstance(dataset, pd.DataFrame)
+        self.assertEqual(len(dataset), 4)
+        self.assertEqual(len(dataset.query('filename == "/path/to/file_1.java" and is_true_emo == True')), 1)
+        self.assertEqual(dataset.query('filename == "/path/to/file_1.java" and is_true_emo == True').true_range[0], '[40, 60]')
+        self.assertEqual(len(dataset.query('filename == "/path/to/file_2.java" and is_true_emo == True')), 0)
 
     def test_drop_duplicates_by_filename(self):
         df = pd.DataFrame({'output_filename': ['fn_1', 'fn_2', 'fn_2', 'fn_3']})
